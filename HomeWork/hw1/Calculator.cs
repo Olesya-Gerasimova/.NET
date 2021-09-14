aaea994223c42b6747e92ce6747429c55ffd56c9
Olesya-Gerasimova <ogerasimova966@gmail.com>
2021-09-14 20:56:00 +0300

    Added code Home work 1 Calculator

---
+++ HomeWork/hw1/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace hw1
+{
+    public class Calculator
+    {
+        public int Calculate(string operation, int val1, int val2)
+        {
+            var result = operation switch
+            {
+                "+" => val1 + val2,
+                "-" => val1 - val2,
+                "*" => val1 * val2,
+                "/" => val1 / val2,
+                _ => 0
+            };
+            return result;
+        }
+    }
+}
---
+++ HomeWork/hw1/CalculatorLauncher.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace hw1
+{
+    public class CalculatorLauncher
+    {
+        private const int WrongArgumentLength = 1;
+        private const int WrongArgFormat = 2;
+
+        public int Launch(string[] args)
+        {
+            if (CheckArgLength(args))
+            {
+                return WrongArgumentLength;
+            }
+
+            var operation = args[1];
+            if (TryParseOrQuit(args[0], out var val1)
+                || TryParseOrQuit(args[2], out var val2))
+            {
+                return WrongArgFormat;
+            }
+
+            var result = new Calculator().Calculate(operation, val1, val2);
+
+            Console.WriteLine($"Result is: {result}");
+            return 0;
+        }
+
+        private static bool TryParseOrQuit(string arg, out int val1)
+        {
+            var isVal1 = int.TryParse(arg, out val1);
+            if (isVal1)
+            {
+                return false;
+            }
+
+            Console.WriteLine($"Value is not int: {arg}");
+            {
+                return true;
+            }
+
+        }
+
+        private static bool CheckArgLength(string[] args)
+        {
+            if (args.Length >= 3)
+            {
+                return false;
+            }
+
+            Console.WriteLine(
+                $"The program requires 3 "
+                + $"CLI arguments but {args.Length} provided");
+            {
+                return true;
+            }
+        }
+    }
+}
