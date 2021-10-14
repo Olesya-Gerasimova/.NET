72da4020b90e888fa7508735a9848805ba865e45
Olesya-Gerasimova <ogerasimova966@gmail.com>
2021-10-14 13:29:06 +0300

    Added TryParse

--- HomeWork/hw3/Program.fs
+++ HomeWork/hw3/Program.fs
@@ -1,13 +1,21 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
+//
 open System
 
 type Program() =
     static member Main(args : string[]) = 
         if args.Length > 3
-        then failwith "Wrong number of arguments >3"
-        let a : int = int32 args.[0]
-        let b : int = int32 args.[2]
+            then failwith "Wrong number of arguments >3"
+        
+        let TryParseOrQuit (s:string) =
+            match Int32.TryParse s with
+            | (true, number) -> number
+            | _              -> failwith "sorry - you did not enter an integer"
+        
+        let a : int = TryParseOrQuit args.[0]
+        let b : int = TryParseOrQuit args.[2]
+
+        
         let addition x y = x + y
         let subtraction x y = x - y
         let multiply x y = x * y
@@ -16,11 +24,15 @@ type Program() =
         | "+" -> printfn($"The result is: %d{addition a b}")
         | "-" -> printfn($"The result is: %d{subtraction a b}")
         | "*" -> printfn($"The result is: %d{multiply a b}")
-        | "/" -> printfn($"The result is: %d{divide a b}")
+        | "/" ->
+            try
+                printfn($"The result is: %d{divide a b}")
+            with
+            | :? DivideByZeroException -> failwith "Cannot divide by 0"
         | _ -> failwith "Unsupported operation type"
 
 module Program__run =
     [<EntryPoint>]
     let main args = 
         Program.Main (args)
-        0        
+        0
