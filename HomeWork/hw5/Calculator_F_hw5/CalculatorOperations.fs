178e779922d33d2e5ab9f3d65a5bc6d4059b65f0
Olesya-Gerasimova <ogerasimova966@gmail.com>
2021-10-21 01:18:20 +0300

    added result computation expression and tests

---
+++ HomeWork/hw5/Calculator_F_hw5/CalculatorOperations.fs
@@ -0,0 +1,37 @@
+﻿namespace Calculator_F_hw5
+
+
+
+module public CalculatorOperations =
+    [<Literal>]
+    let Addition = "+"
+
+    [<Literal>]
+    let Subtract = "-"
+
+    [<Literal>]
+    let Multiply = "*"
+
+    [<Literal>]
+    let Divide = "/"
+
+    [<Literal>]
+    let NotIntegerErrorMassage = "Argument is not numbers or  long"
+
+    [<Literal>]
+    let OperationErrorMassage = "Operation is not correct"
+
+    [<Literal>]
+    let ArgumentErrorMassage = "wrong number of arguments"
+
+    [<Literal>]
+    let Ok_Code = 0
+
+    [<Literal>]
+    let OperationErrorCode = 1
+
+    [<Literal>]
+    let NumbersErrorCode = 2
+
+    [<Literal>]
+    let ArgumentErrorCode = 3
---
+++ HomeWork/hw5/Calculator_F_hw5/MyCalculator.fs
@@ -0,0 +1,79 @@
+﻿namespace Calculator_F_hw5
+
+open System
+open CalculatorOperations
+
+type ErrorBuilder() =
+
+    member this.Bind(x, f) =
+        match x with
+        | Ok x -> f x
+        | Error _ -> x
+
+    member b.Combine(x, f) = f x
+
+    member this.Return(x) = Ok x
+
+module public MyCalculator =
+    let SetError (error: string) = printfn $"%s{error}"
+
+    let maybe = ErrorBuilder()
+
+    let CheckNumber (a: String) =
+        let f, _ = Decimal.TryParse a
+
+        if f = true then
+            Ok Ok_Code
+        else
+            SetError(NotIntegerErrorMassage)
+            Error NumbersErrorCode
+
+    let CheckOperation (operation: String) =
+        match operation with
+        | Addition
+        | Subtract
+        | Multiply
+        | Divide -> Ok Ok_Code
+        | _ ->
+            SetError(OperationErrorMassage)
+            Error OperationErrorCode
+
+    let Add (val1: string, val2: string) : int =
+        let r = Decimal.Parse val1 + Decimal.Parse val2
+        let result = "Result = " + r.ToString("G29")
+        printfn $"{result}"
+        Ok_Code
+
+    let Sub (val1: string, val2: string) : int =
+        let r = Decimal.Parse val1 - Decimal.Parse val2
+        let result = "Result = " + r.ToString("G29")
+        printfn $"{result}"
+        Ok_Code
+
+    let Mult (val1: string, val2: string) : int =
+        let r = Decimal.Parse val1 * Decimal.Parse val2
+        let result = "Result = " + r.ToString("G29")
+        printfn $"{result}"
+        Ok_Code
+
+    let Div (val1: string, val2: string) : int =
+        let r = Decimal.Parse val1 / Decimal.Parse val2
+        let result = "Result = " + r.ToString("G29")
+        printfn $"{result}"
+        Ok_Code
+
+    let Proces (num1: string, oper: string, num2: string) : int =
+        match oper with
+        | Addition -> Add(num1, num2)
+        | Subtract -> Sub(num1, num2)
+        | Multiply -> Mult(num1, num2)
+        | Divide -> Div(num1, num2)
+        | _ -> failwith "todo"
+
+    let getCodeForReturn (num1: String, op: String, num2: String) : Result<int, int> =
+        maybe {
+            let! _num1 = CheckNumber num1
+            let! _opr = CheckOperation op
+            let! _num2 = CheckNumber num2
+            return Proces(num1, op, num2)
+        }
